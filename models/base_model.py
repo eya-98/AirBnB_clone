9d50481dfaf51c9534bed38297a8cf961b410ad2
eya-98 <eya.nani@sesame.com.tn>
2021-02-16 11:04:05 +0000

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 forma = "%Y-%m-%dT%H:%M:%S.%f"
 
+
 class BaseModel:
     """defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
@@ -18,17 +19,24 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    
+
     def __str__(self):
         """print the id and the dict"""
-        return ("[{}], ({}), {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}], ({}), {}".format(self.__class__.__name__,
+                                        self.id, self.__dict__))
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """
+        updates the public instance
+        attribute updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """
+        returns a dictionary containing
+        all keys/values of __dict__ of the instance
+        """
         dic = dict(self.__dict__)
         dic['__class__'] = self.__class__.__name__
         dic['updated_at'] = self.updated_at.isoformat()
