f7afddbce6b9da454ed87b6857314a99ec6685e3
eya-98 <eya.nani@sesame.com.tn>
2021-02-17 16:59:42 +0000

    base model file

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            
+
     def __str__(self):
         """print the id and the dict"""
         return ("[{}], ({}), {}".format(self.__class__.__name__,
