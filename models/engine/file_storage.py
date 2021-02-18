b4deb2133c8782a18b5e426c4bd49274f03d8928
eya-98 <eya.nani@sesame.com.tn>
2021-02-18 12:29:40 +0000

    storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """holberton Module"""
+import models
 import json
 
 class FileStorage:
@@ -9,16 +10,21 @@ class FileStorage:
     
     def all(self):
         """ returns the dictionary __objects"""
-        return self.__object
+        return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__object[self.__class__name__ + '.' + self.id] = obj
-        
+        class_name = type(obj).__name__
+        key_obj = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key_obj] = obj
+
     def save(self):
         """serializes __objects to the JSON file"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w') as f:
-            json.dump(self.__object, f)
+            json.dump(json_objects, f)
 
     def reload(self):
         """deserializes the JSON file"""
