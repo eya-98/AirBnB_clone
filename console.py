b730695f02e65aa2f2195fbe5eeb67a56a0090b8
eya-98 <eya.nani@sesame.com.tn>
2021-02-18 22:24:57 +0000

    console

--- console.py
+++ console.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """Holberton Module"""
-
 import cmd
 from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     """define HBNBCOMMand as a class"""
     prompt = '(hbnb)'
 
     def do_EOF(self, line):
-         """implement EOF method"""
-         return True
+        """implement EOF method"""
+        return True
 
-    def do_exit(self, *args):
+    def do_quit(self, args):
         """implement quit"""
-        return True         
-    
+        return True
 
     def emptyline(self):
         """implement empty line"""
         pass
-        
-    if __name__ == '__main__':
-        HBNBCommand().cmdloop()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
